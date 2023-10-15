4e6d2527acc6edfdb751490352256420408a7f5a
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-10-15 20:55:58 +0300

    feat: + get current user endpoint

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
 import {
   ClassSerializerInterceptor,
   Controller,
+  Get,
   Post,
+  Req,
   UseInterceptors,
 } from '@nestjs/common';
 import { SignUpRequestDto } from './dtos/sign-up-request.dto';
@@ -9,11 +11,23 @@ import { SignInRequestDto } from './dtos/sign-in-request.dto';
 import { Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Public } from './decorators/public.decorator';
+import { UserService } from 'src/user/user.service';
+import { Request } from 'express';
+import { User } from 'src/user/entities/user.entity';
+
+declare module 'express' {
+  export interface Request {
+    user?: User;
+  }
+}
 
 @UseInterceptors(ClassSerializerInterceptor)
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
 
   @Public()
   @Post('sign-up')
@@ -25,5 +39,10 @@ export class AuthController {
   @Post('sign-in')
   signIn(@Body() signInDto: SignInRequestDto) {
     return this.authService.signIn(signInDto);
+  }
+
+  @Get('current-user')
+  getCurrentUser(@Req() request: Request) {
+    return this.userService.findOne(request.user.id);
   }
 }
